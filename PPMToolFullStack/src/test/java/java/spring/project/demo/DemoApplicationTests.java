3a78f3a93f2198e8b666d52bbcdeab72187afcef
VineethDvv <42659076+VineethDvv@users.noreply.github.com>
2020-04-22 11:50:52 -0500

    first commit

---
+++ PPMToolFullStack/src/test/java/java/spring/project/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.spring.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
